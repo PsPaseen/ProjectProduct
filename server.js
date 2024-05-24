892ec2b7f3faf450cc407fb7cb81aeebfc66980e
PUNNAz <148584383+PUNNAz@users.noreply.github.com>
2024-05-24 14:41:50 +0700

    upload data (backend)

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import sql from "mssql";
 import path from 'path';
 import fs from 'fs';
+import multer from 'multer';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -49,6 +50,54 @@ async function testConnection() {
     })
   }
 )
+
+const storage = multer.diskStorage({
+  destination:(req,file ,cb)=>{
+    cb(null,'upload/');
+  },
+  filename:(req,file,cb)=>{
+    const timestamp = Date.now();
+    const randomValue = Math.random().toString();
+    const hash = crypto.createHash('sha256').update(timestamp + randomValue).digest('hex');
+    const newFileName = `${hash}.png`; // Attach ".png"
+    cb(null, newFileName);
+  }
+})
+
+const upload = multer({ storage: storage });
+
+app.post('/uploadproduct',upload.single('picture'),async(req,res)=>{
+
+  const { Productname, Stock, Productdetail, Productprice } = req.body;
+
+  const Pathpic = req.file ? req.file.path : null;
+
+  console.log('Received form data:', { Productname, Stock, Productdetail, Productprice });
+  console.log('Received picture:', Pathpic);
+
+  try{
+    const pool = await sql.connect(config);
+
+    const inputdata = await pool.request()
+    .input('Productname' ,sql.NVarChar, Productname)
+    .input('Stock' , sql.Int , Stock)
+    .input('Productdetail' ,sql.NVarChar , Productdetail)
+    .input('Productprice' ,sql.Money , Productprice)
+    .input('Pathpic' , sql.NVarChar , Pathpic )
+    .query(`
+    INSERT INTO RProduct (ProductName,Stock,Productdetail,Productprice,Pathpic)
+    VALUES (@Productname,@Stock,@Productdetail,@Productdetail,@Pathpic)
+    `)
+
+    console.log('Rows affected:', inputdata.rowsAffected);
+
+    await pool.close();
+
+    res.send('Form data received and inserted into the database successfully!');
+  }catch(error){
+    console.error('Error inserting data into the database:', error.message);
+  }
+})
 
 app.get('/image', (req, res) => {
   const filename = "test01.jpg";
